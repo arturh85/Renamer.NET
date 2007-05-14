1347e221bb63e5528c3a79f040f87605a871a68e
dummy <lwagner@gmx.de>
2007-05-14 17:04:54 +0000

    added testFiles

---
+++ testFiles/create.cmd
@@ -0,0 +1,5 @@
+echo > The.Simpsons.S18E10.PDTV.XviD-NoTV.av
+echo > The.Simpsons.S18E12.PDTV.XviD-LOL.avi
+echo > The.Simpsons.S18E13.PDTV.XviD-2HD.avi
+echo > The.Simpsons.S18E14.PDTV.XviD-LOL.avi
+echo > The.Simpsons.S18E16.PDTV.XviD-LOL.avi
